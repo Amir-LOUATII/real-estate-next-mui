52e119cd6194228cda34b954b7202520b34d0151
Amir_Louati25 <ameer.louati@gmail.com>
2025-06-05 18:47:03 +0100

    feat: enhance AppProviders component with ThemeProvider configuration

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,5 +2,14 @@ import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 
 export default function AppProviders({ children }: { children: ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
 }
