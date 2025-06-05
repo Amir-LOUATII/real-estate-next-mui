3e2633492fdfab1e46d488c1010421871cc66ac6
Amir_Louati25 <ameer.louati@gmail.com>
2025-06-05 14:30:15 +0100

    remove AppRouterCacheProvider from AppProviders component

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,5 @@
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ReactNode } from "react";
 
 export default function AppProviders({ children }: { children: ReactNode }) {
-  return <AppRouterCacheProvider>{children}</AppRouterCacheProvider>;
+  return <>{children}</>;
 }
